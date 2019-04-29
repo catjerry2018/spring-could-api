e9a7a65bd7a136bdbabba6a823a78e9158f83e44
catjerry2018 <48075969+catjerry2018@users.noreply.github.com>
2019-04-29 12:00:57 +0800

    CORS跨域解决

--- eureka-zuul/src/main/java/com/xfy/apiall/eureka_zuul/Application.java
+++ eureka-zuul/src/main/java/com/xfy/apiall/eureka_zuul/Application.java
@@ -4,11 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication(scanBasePackages = {"com.xfy.apiall.eureka_zuul"})
 @EnableZuulProxy
 @RefreshScope
 public class Application {
+    @Bean
+    public CorsFilter corsFilter() {//解决跨域访问
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(18000L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
